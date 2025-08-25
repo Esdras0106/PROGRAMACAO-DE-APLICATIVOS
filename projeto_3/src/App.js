d6198c4d92874afbc71bd51a69d44cef41c0f799
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-25 16:52:01 -0300

    salvar

---
+++ projeto_3/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import AdicionarProduto from './components/AdicionarProduto';
+import ListaDeProdutos from './components/ListaDeProdutos';
+import logo from '../src/assets/images/logo_estilo.png'
+
+function App() {
+  const [produtos, setProdutos] = useState(['Mouse', 'Teclado', 'Monitor']);
+
+  const adicionarUsuario = (nome) => {
+    const produto = nome.trim();
+
+    if (produtos.includes(produto)) {
+      alert('O produto já existe!');
+      return;
+    }  
+    setProdutos([...produtos, produto]);
+  };
+  
+  return (
+    <div className="App">
+      <img src = {logo} className='logo' alt='Logo Estilo'></img>
+      <header>
+      <h1>Produtos</h1>
+      <AdicionarProduto onAdd={adicionarUsuario} />
+      <hr />
+      <h2>Lista de produtos</h2>
+      <ListaDeProdutos itens={produtos} />
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
